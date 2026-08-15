e47b45700b5c3d84fa56b8ff5c2ace786181a9e0
Orkun Eren Aydın <150009962+OrkunEren@users.noreply.github.com>
2026-08-15 21:14:35 +0300

    Add player visual id

---
+++ Source/Player/Visuals/PlayerVisualId.cs
@@ -0,0 +1,10 @@
+namespace InvadersOverboard.Player.Visuals
+{
+    public enum PlayerVisualId : byte
+    {
+        Cyclops = 0,
+        FourEyed = 1,
+        Lizard = 2,
+        Short = 3
+    }
+}
